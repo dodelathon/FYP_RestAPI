b3282ed90b95797e9e44e256d66224b5d94a5c9a
Dodlethon <donal_doherty@live.co.uk>
2019-09-27 22:13:45 +0100

    Fixing potetential bottleneck/ Slowing down the image transfer

--- FYP_TestAPI/FYP_TestAPI/wwwroot/site.js
+++ FYP_TestAPI/FYP_TestAPI/wwwroot/site.js
@@ -139,5 +139,5 @@ function refreshIt()
 	timestamp = (new Date()).getTime(),
 	newUrl = source + '?_=' + timestamp;
 	document.getElementById("Stream").src = newUrl;
-	setTimeout(refreshIt, 1000);
+	setTimeout(refreshIt, 2000);
 }
