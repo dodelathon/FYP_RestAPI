fac74ff6db27be7f4e30b304644808d16228294b
Dodlethon <donal_doherty@live.co.uk>
2020-01-03 17:54:54 +0000

    Cs4227 removed

--- FYP_TestAPI/FYP_TestAPI/Program.cs
+++ FYP_TestAPI/FYP_TestAPI/Program.cs
@@ -13,7 +13,9 @@ namespace FYP_TestAPI
     public class Program
     {
         public static void Main(string[] args)
-        {  }
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
